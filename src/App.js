197f6b6fdaa893a1a5d77d26f2e6af9e36bd2802
Lukas Prochazka <prochazka.lu@gmail.com>
2023-03-20 12:44:23 +0100

    Change url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { useState } from 'react';
 function App() {
   const [text, setText] = useState('');
 
+  const imageUrl =
+    text === ''
+      ? 'https://api.memegen.link/images/doge.png'
+      : `https://api.memegen.link/images//${text}.png`;
+
   return (
     <div className="App">
       <label>
@@ -13,21 +18,7 @@ function App() {
           onChange={(event) => setText(event.currentTarget.value)}
         />
       </label>
-      {text === '' ? (
-        <img
-          src={`https://api.memegen.link/images/doge.png`}
-          alt=""
-          width={200}
-          height={200}
-        />
-      ) : (
-        <img
-          src={`https://api.memegen.link/images//${text}.png`}
-          alt=""
-          width={200}
-          height={200}
-        />
-      )}
+      <img src={imageUrl} alt="" width={200} height={200} />
     </div>
   );
 }
